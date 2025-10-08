68bbf8226c3704e20d70c94667ab4a895356538b
adventaa <119681406+adventaa@users.noreply.github.com>
2025-10-08 10:08:09 +0700

    Update Retailer Analysis.sql

--- Queries/Retailer Analysis.sql
+++ Queries/Retailer Analysis.sql
@@ -4,10 +4,8 @@ SELECT Retailer,
        SUM(Total_Sales) AS Total_Penjualan,
        YEAR(InvoiceDate) AS Tahun
 FROM adidas_sales
-WHERE YEAR(InvoiceDate) IN (2020)
 GROUP BY Tahun, Retailer
-ORDER BY Tahun, Total_Penjualan DESC
-LIMIT 5;
+ORDER BY Tahun, Total_Penjualan DESC;
 
 -- Menampilkan persebaran retailer di berbagai daerah dan negara --
 SELECT DISTINCT Retailer, Region, State
